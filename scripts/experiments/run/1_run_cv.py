92824694b1f549b9bda231312fa8976eeec58e1a
Vitor Cerqueira <vcerq@Vitors-MacBook-Air.local>
2026-09-18 09:06:08 +0100

    ins

--- scripts/experiments/run/1_run_cv.py
+++ scripts/experiments/run/1_run_cv.py
@@ -8,6 +8,9 @@ from neuralforecast import NeuralForecast
 from src.loaders import ChronosDataset, LongHorizonDatasetR
 from src.neuralnets import BaseModelsConfig
 
+
+# merge with 2_ and add insample
+
 warnings.filterwarnings("ignore")
 
 # ---- data loading and partitioning
